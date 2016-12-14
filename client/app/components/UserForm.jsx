e98e9bb22be53973b7bdc54bf5c390376861e4d4
Xm4s <marco.natale.colombo@gmail.com>
2016-12-14 19:56:51 +0100

    Update form input names to match the resource (user)

--- client/app/components/UserForm.jsx
+++ client/app/components/UserForm.jsx
@@ -14,31 +14,39 @@ export default class UserForm extends React.Component {
 
   toOption = type => <option key={type} value={type}>{type}</option>
 
+  onSubmit(e) {
+    e.preventDefault();
+    const data = new FormData(this.userForm);
+    console.log('form data', $(this.userForm).serialize());
+    // send data to the server via ajax, collect the errors
+  }
+
   render() {
     return (
-      <form>
+      <form ref={(form) => this.userForm = form } onSubmit={this.onSubmit.bind(this)}>
         <h1>New User</h1>
         <label>Full Name</label>
-        <input type="text" name="fullname"/>
+        <input type="text" name="user[fullname]"/>
         <label>Email</label>
-        <input type="text" name="email"/>
-        <h2>Skills</h2>
-        <label>Name</label>
-        <input type="text" name=""/>
-        <label>Points</label>
-        <input type="number" name=""/>
-        <label>Type</label>
-        <select name="">{this.typeOptions}</select>
-        <br/>
-        <label>Name</label>
-        <input type="text" name=""/>
-        <label>Points</label>
-        <input type="number" name=""/>
-        <label>Type</label>
-        <select name="">{this.typeOptions}</select>
-        <br/>
+        <input type="text" name="user[email]"/>
+
+        {this.renderSkill(0)}
+
         <input type="submit" value="Create User"/>
       </form>
+    );
+  }
+
+  renderSkill(index) {
+    return (
+      <div>
+        <label>Name</label>
+        <input type="text" name={`user[skill_attributes][${index}][name]`}/>
+        <label>Points</label>
+        <input type="number" name={`user[skill_attributes][${index}][points]`}/>
+        <label>Type</label>
+        <select name={`user[skill_attributes][${index}][type]`}>{this.typeOptions}</select>
+      </div>
     );
   }
 }
